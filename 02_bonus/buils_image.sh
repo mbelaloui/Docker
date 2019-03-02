533f06f8ad8051b472660a8ca6a7df64e39af50f
Mehdi aissa BELALOUI <mbelalou@e3r3p8.42.fr>
2019-03-02 12:49:53 +0100

    02-03-2019

---
+++ 02_bonus/buils_image.sh
@@ -0,0 +1,4 @@
+docker image build -t ft-vim ../01_dockerfiles/ex00/
+docker image build -t ft-teamserveur ../01_dockerfiles/ex01/
+docker image build -t ft-rails ../01_dockerfiles/ex02/
+docker image build -t ft-guitlab ../01_dockerfiles/ex03/
