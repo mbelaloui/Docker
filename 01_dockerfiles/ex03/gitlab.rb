94a7c635ef934489f1f59aa33999dd1c7b7fe832
root <admin@example.com>
2019-03-15 18:30:15 +0100

    15-03-2019

--- 01_dockerfiles/ex03/gitlab.rb
+++ 01_dockerfiles/ex03/gitlab.rb
@@ -1,2 +1,5 @@
-external_url "https://gitlab.example.com"
+external_url "https://192.168.99.100"
+#letsencrypt['enable'] = false
 nginx['redirect_http_to_https'] = true
+nginx['ssl_certificate'] = "/etc/gitlab/ssl/192.168.99.100.crt"
+nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/192.168.99.100.key"
--- 02_bonus/buils_image.sh
+++ 02_bonus/buils_image.sh
@@ -1,4 +1,10 @@
 docker image build -t ft-vim ../01_dockerfiles/ex00/
 docker image build -t ft-teamserveur ../01_dockerfiles/ex01/
-docker image build -t ft-rails ../01_dockerfiles/ex02/
-docker image build -t ft-guitlab ../01_dockerfiles/ex03/
+docker image build -t ft-rails:on-build ../01_dockerfiles/ex02/
+docker image build -t ftrails ../01_dockerfiles/ex02/run/
+docker image build -t ft-gitlab ../01_dockerfiles/ex03/
+
+docker image build -t chat ./chat/
+docker image build -t hellow_java ./java_hellow/
+docker image build -t push_swap ./push_swap/
+docker image build -t react ./react/
